a4775368c94bec494cc68cf6ca3ec8bff35f1ffb
monieshravichandrran <monieshravichandrran@gmail.com>
2021-11-15 18:34:51 +0530

    Setup Commit

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors);
+
+app.listen(8000, () => {
+    console.log('Listening on Port: 8000');
+})
