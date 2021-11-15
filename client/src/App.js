4082f2df179bb923666d437a4d8766b242f8f2a8
monieshravichandrran <monieshravichandrran@gmail.com>
2021-11-15 19:54:21 +0530

    adding pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
-
+import Home from "./pages/Home";
+import Add from "./pages/Add";
+import Update from "./pages/Update";
+import Delete from "./pages/Delete";
+import {BrowserRouter as Router,Route} from "react-router-dom"
 const App = () => {
   return (
     <>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/add" component={Add} />
+        <Route exact path="/update" component={Update} />
+        <Route exact path="/delete" component={Delete} />
+      </Router>
     </>
   )
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+    return (
+        <>
+            <div className="h-screen w-screen">
+                <Navbar/>
+            </div>
+        </>
+    )
+}
+
+export default Home;
